728fb3d2472e4ed9b93afc51c8af23a57905bc82
Kumar Shaswat <er.kumar.shaswat@gmail.com>
2021-05-12 19:53:09 +0530

    hashing and salting with rounds using bcrypt

--- authentication_security/Secrets - Starting Code/app.js
+++ authentication_security/Secrets - Starting Code/app.js
@@ -4,7 +4,11 @@ require('dotenv').config()
 const express = require('express');
 const ejs = require ('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
+// const md5 = require('md5')
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+
 
 //inintialize express
 const app = express()
@@ -30,7 +34,7 @@ const userSchema = new mongoose.Schema({
 
 // // encryption setup
 secret = process.env.SECRET
-userSchema.plugin(encrypt, { secret:secret, encryptedFields:["password"] });
+// userSchema.plugin(encrypt, { secret:secret, encryptedFields:["password"] });
 
 // //  Mongo model
 const User = new mongoose.model("User", userSchema);
@@ -52,13 +56,15 @@ app.post("/login", function(req, res){
         if(err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets")
-                }else {
-                    
+                if (foundUser) {
+                    bcrypt.compare(password, foundUser.password, function(err, result) {
+                        if (result==true){
+                            res.render('secrets')
+                        }else{
+                            console.log(err);
+                        }
+                    });
                 }
-            }
         }
     })
 })
@@ -68,18 +74,23 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else {
-            res.render("secrets")
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else {
+                res.render("secrets")
+            }
+        })
+    
     })
+    
+    
 })
 
 
